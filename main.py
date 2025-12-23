945f1013d03a87a26330733e58287787fe721c44
Vu hoang long <longvulinhhoang@gmail.com>
2025-12-23 16:51:37 +0700

    Code in develop branch after rebasing

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ def another_code_in_main():
 
 def develop_test_rebase():
     print("Develop test rebase")
+
+def continue_code_in_develop_after_rebase():
+    print("Continue code in develop after rebase")
